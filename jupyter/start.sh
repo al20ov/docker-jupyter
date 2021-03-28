2bd11162f66e924fb816555f13f6fbe703f1ea26
al20ov <nicolas.antauf@gmail.com>
2021-03-28 23:52:11 +0000

    [UPDATE]: migrated to a docker-compose

---
+++ jupyter/start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+password="${JUPYTER_PASSWORD:-'password'}"
+
+hashed_password=`python -c "from notebook.auth import passwd; print(passwd('$password'))"`
+
+jupyter notebook \
+  --ip=0.0.0.0 \
+  --port=8080 \
+  --allow-root \
+  --NotebookApp.password="$hashed_password" \
+  --NotebookApp.notebook_dir="/root/notebooks"
